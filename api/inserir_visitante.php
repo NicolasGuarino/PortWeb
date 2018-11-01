478d6b8bf61195bd25495cb15a96671109f377ca
Mateus Ferreira Morelli <mateus.ferreira@primi.local>
2018-11-01 16:05:55 -0300

    Adicionando retorno dos dados do usuario na API de insercao de visitante

--- api/inserir_visitante.php
+++ api/inserir_visitante.php
@@ -20,29 +20,39 @@
 		$valores = $tipo_usuario_id . ", now(), '" . $nome . "', '" . $foto_usuario . "', '" . $rg . "', 1";	// Valores a serem inseridos
 		$query   = "insert into usuario(". $campos .") values(". $valores .");";								// Query completa
 
-		// Executando a query
-		mysqli_query($con, $query);
-
-		// Recuperando o ID do usuário
-		$usuario_id = mysqli_insert_id($con);
+		mysqli_query($con, $query);				// Executando a query
+		$usuario_id = mysqli_insert_id($con);	// Recuperando o ID do usuário
 
 		// Inserindo na tabela de 'veículo'
 		$campos  = "placa, foto, ativo";											// Campos da tabela
 		$valores = "'" . $placa . "', '" . $foto_veiculo . "', 1";					// Valores a serem inseridos
 		$query   = "insert into veiculo(". $campos .") values(". $valores .");";	// Query completa
 
-		// Executando a query
-		mysqli_query($con, $query);
-
-		// Recuperando o ID do veículo
-		$veiculo_id = mysqli_insert_id($con);
+		mysqli_query($con, $query);				// Executando a query
+		$veiculo_id = mysqli_insert_id($con);	// Recuperando o ID do veículo
 
 		// Inserindo na tabela de relacionamento entre usuário e veículo
 		$campos  = "usuario_id, veiculo_id";													// Campos da tabela
 		$valores = $usuario_id . ", " . $veiculo_id;											// Valores a serem inseridos
 		$query   = "insert into rel_usuario_veiculo(". $campos .") values(". $valores .");";	// Query completa
 
-		// Executando a query
-		$exec = mysqli_query($con, $query);
+		$exec = mysqli_query($con, $query);		// Executando a query
+
+		// Consultando informações do usuário
+		$query  = "select u.usuario_id, u.nome, u.rg, v.veiculo_id, v.placa from usuario as u ";
+		$query .= "inner join rel_usuario_veiculo as uv on(u.usuario_id = uv.usuario_id) ";
+		$query .= "inner join veiculo as v on(v.veiculo_id = uv.veiculo_id) ";
+		$query .= "where u.usuario_id = ". $usuario_id .";";
+
+		$exec = mysqli_query($con, $query);		// Executando a query
+
+		// Lista de usuário
+		$lista_usuario = [];
+
+		// Preenchendo a lista de usuario
+		while($usuario = mysqli_fetch_array($exec)) $lista_usuario[] = $usuario;
+
+		// Transformando em JSON
+		echo json_encode($lista_usuario);
 
 	}else echo "Nome e RG não enviado(s)";
