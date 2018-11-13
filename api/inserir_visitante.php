04fcfe670cb75dafec339e312479583c482f3869
Nicolas Guarino Santana <nicolas.guarino@primi.local>
2018-11-13 10:29:18 -0200

    Modificando API de visitante para verificar se o RG já existe

--- api/inserir_visitante.php
+++ api/inserir_visitante.php
@@ -12,59 +12,67 @@
 	$foto_usuario	 = "img/icone/person.png";		// Imagem padrão
 	$foto_veiculo	 = "img/icones/ic_carro.png";	// Imagem padrão
 
-	// Consultando o primeiro documento disponível para condômeno
-	$query  = "select documento_id from documento ";
-	$query .= "where disponibilidade = 1 and numero_etiqueta like 'C%'";
-	$query .= "order by documento_id asc limit 1;";
+	
+	$query = "select * from usuario where rg = '$rg';";
+	$exec = mysqli_query($con, $query);
+	$usuario_id = mysqli_fetch_array($exec)['usuario_id'];
+	if($usuario_id == "" || $usuario_id == null){
 
-	$exec 	   	  = mysqli_query($con, $query);					// Executando a query
-	$documento_id = mysqli_fetch_array($exec)['documento_id'];	// Recuperando o ID do documento
-
-	// Atualizando a disponibilidade do documento (para '0')
-	$query  = "update documento set disponibilidade = '0' ";
-	$query .= "where documento_id = " . $documento_id;
-
-	$exec = mysqli_query($con, $query);		// Executando a query
-
-	// Inserindo na tabela de 'usuário'
-	$campos  = "tipo_usuario_id, documento_id, ultima_atualizacao, nome, foto, rg, ativo";								// Campos da tabela
-	$valores = $tipo_usuario_id . ", " . $documento_id . ", now(), '" . $nome . "', '" . $foto_usuario . "', '" . $rg . "', 1";	// Valores a serem inseridos
-	$query   = "insert into usuario(". $campos .") values(". $valores .");";								// Query completa
-
-	mysqli_query($con, $query);				// Executando a query
-	$usuario_id = mysqli_insert_id($con);	// Recuperando o ID do usuário
-
-	// Inserindo veículo somente se houver placa 
-	if($placa != ""){
-
-		// Consultando o primeiro documento disponível para automovel
+		
+		// Consultando o primeiro documento disponível para condômeno
 		$query  = "select documento_id from documento ";
-		$query .= "where disponibilidade = 1 and numero_etiqueta like 'A%'";
+		$query .= "where disponibilidade = 1 and numero_etiqueta like 'C%'";
 		$query .= "order by documento_id asc limit 1;";
 
-		$exec 	   	  		  = mysqli_query($con, $query);					// Executando a query
-		$documento_veiculo_id = mysqli_fetch_array($exec)['documento_id'];	// Recuperando o ID do documento
+		$exec 	   	  = mysqli_query($con, $query);					// Executando a query
+		$documento_id = mysqli_fetch_array($exec)['documento_id'];	// Recuperando o ID do documento
 
 		// Atualizando a disponibilidade do documento (para '0')
 		$query  = "update documento set disponibilidade = '0' ";
-		$query .= "where documento_id = " . $documento_veiculo_id;
-
-		mysqli_query($con, $query); 	// Executando a query
-
-		// Inserindo na tabela de 'veículo'
-		$campos  = "documento_id, placa, foto, ativo";											// Campos da tabela
-		$valores = "'".$documento_veiculo_id."', '" . $placa . "', '" . $foto_veiculo . "', 1";					// Valores a serem inseridos
-		$query   = "insert into veiculo(". $campos .") values(". $valores .");";	// Query completa
-
-		mysqli_query($con, $query);				// Executando a query
-		$veiculo_id = mysqli_insert_id($con);	// Recuperando o ID do veículo
-
-		// Inserindo na tabela de relacionamento entre usuário e veículo
-		$campos  = "usuario_id, veiculo_id";													// Campos da tabela
-		$valores = $usuario_id . ", " . $veiculo_id;											// Valores a serem inseridos
-		$query   = "insert into rel_usuario_veiculo(". $campos .") values(". $valores .");";	// Query completa
+		$query .= "where documento_id = " . $documento_id;
 
 		$exec = mysqli_query($con, $query);		// Executando a query
+
+		// Inserindo na tabela de 'usuário'
+		$campos  = "tipo_usuario_id, documento_id, ultima_atualizacao, nome, foto, rg, ativo";								// Campos da tabela
+		$valores = $tipo_usuario_id . ", " . $documento_id . ", now(), '" . $nome . "', '" . $foto_usuario . "', '" . $rg . "', 1";	// Valores a serem inseridos
+		$query   = "insert into usuario(". $campos .") values(". $valores .");";								// Query completa
+
+		mysqli_query($con, $query);				// Executando a query
+		$usuario_id = mysqli_insert_id($con);	// Recuperando o ID do usuário
+
+		// Inserindo veículo somente se houver placa 
+		if($placa != ""){
+
+			// Consultando o primeiro documento disponível para automovel
+			$query  = "select documento_id from documento ";
+			$query .= "where disponibilidade = 1 and numero_etiqueta like 'A%'";
+			$query .= "order by documento_id asc limit 1;";
+
+			$exec 	   	  		  = mysqli_query($con, $query);					// Executando a query
+			$documento_veiculo_id = mysqli_fetch_array($exec)['documento_id'];	// Recuperando o ID do documento
+
+			// Atualizando a disponibilidade do documento (para '0')
+			$query  = "update documento set disponibilidade = '0' ";
+			$query .= "where documento_id = " . $documento_veiculo_id;
+
+			mysqli_query($con, $query); 	// Executando a query
+
+			// Inserindo na tabela de 'veículo'
+			$campos  = "documento_id, placa, foto, ativo";											// Campos da tabela
+			$valores = "'".$documento_veiculo_id."', '" . $placa . "', '" . $foto_veiculo . "', 1";					// Valores a serem inseridos
+			$query   = "insert into veiculo(". $campos .") values(". $valores .");";	// Query completa
+
+			mysqli_query($con, $query);				// Executando a query
+			$veiculo_id = mysqli_insert_id($con);	// Recuperando o ID do veículo
+
+			// Inserindo na tabela de relacionamento entre usuário e veículo
+			$campos  = "usuario_id, veiculo_id";													// Campos da tabela
+			$valores = $usuario_id . ", " . $veiculo_id;											// Valores a serem inseridos
+			$query   = "insert into rel_usuario_veiculo(". $campos .") values(". $valores .");";	// Query completa
+
+			$exec = mysqli_query($con, $query);		// Executando a query
+		}
 	}
 
 	// Consultando informações do usuário e veículo
