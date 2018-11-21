d751f553cc217439f5eb9485ac0c6a44c25ff2d0
Gabriel Navevaiko <gabriel@primi.com.br>
2018-11-21 17:16:02 -0200

    Alterado api de inserir empresa

--- api/inserir_empresa.php
+++ api/inserir_empresa.php
@@ -8,20 +8,21 @@
 		$nome 	  = addslashes($_POST['nome']);
 		$telefone = addslashes($_POST['telefone']);
 		$email 	  = addslashes($_POST['email']);
-		$imagem   = $_FILES['imagem'];
-		$caminho  = "img/";
-		$caminho_upload = "../".$caminho.$imagem['name'];
-		$caminho_banco = $caminho.$imagem['name'];
+		// $imagem   = $_FILES['imagem'];
+		// $caminho  = "img/";
+		// $caminho_upload = "../".$caminho.$imagem['name'];
+		$caminho_banco = '';
+		// $caminho_banco = $caminho.$imagem['name'];
 	
-		if(move_uploaded_file($imagem['tmp_name'], $caminho_upload)) {
+		// if(move_uploaded_file($imagem['tmp_name'], $caminho_upload)) {
 			$query  = "insert into empresa(nome, foto, telefone, email)";
 			$query .= "values('".$nome."', '".$caminho_banco."', '".$telefone."', '".$email."');";
 			$result = mysqli_query($conexao, $query) or die("Erro:" . mysqli_error($conexao));
 
 			echo intval($result);
-		}else{
-			echo "Erro: " . $imagem['error'];
-		}
+		// }else{
+			// echo "Erro: " . $imagem['error'];
+		// }
 		
 	}
 
