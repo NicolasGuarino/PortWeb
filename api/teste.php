f3bc5dbaca197cc58183d20e5e4926b40a3f33cf
Mateus Ferreira Morelli <mateus.ferreira@primi.local>
2019-02-05 16:33:16 -0200

    Mudando conexao com o banco de dados para conexao externa ao projeto

--- api/teste.php
+++ api/teste.php
@@ -1,13 +1,4 @@
-<?php 
-	include("../../libs/EnviarEmails/funcao.php");
-
-	$corpo = file_get_contents("../corpo_email/email_recuperar_senha.php");
-		
-	$corpo = str_replace('$nome_usuario', "Gabriel Navevaiko", $corpo);
-	$corpo = str_replace('$id', 15, $corpo);
-	$corpo = str_replace('$email', "gabriel@primi.com.br", $corpo);
-	$parametrosEnvio = array('email_envio' => 'enigma@primi.com.br', 'senha' => 'jeferson@1010', 'nome_envio' => 'Portaria');
-	
-	$retorno = enviarEmail(["gabriel@primi.com.br"], [], "Boas vindas", $corpo, $parametrosEnvio);
-	if($retorno['status'] == 1) $result = 1;
-?>		
+<?php
+	include "../../../conexao_db.php";
+    $conexao = conectar("portaria");
+?>
--- teste.php
+++
@@ -1,5 +0,0 @@
-<?php
-	echo 10 / 3;
-	echo "<br />";
-	echo ceil(10 / 3);
-?>
